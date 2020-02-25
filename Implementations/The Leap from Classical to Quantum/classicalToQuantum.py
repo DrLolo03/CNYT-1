0ad87fde140f16aecc4cebf3dc29d3a779b578c8
Rincon10 <ivan.rincon-s@mail.escuelaing.edu.co>
2020-02-24 20:47:16 -0500

    Latest Commit

--- Implementations/The Leap from Classical to Quantum/classicalToQuantum.py
+++ Implementations/The Leap from Classical to Quantum/classicalToQuantum.py
@@ -9,9 +9,6 @@ def probabilisticSystem( matrix, vectIni, clicks ):
         length  = len( vectIni )
         for x in range(clicks  ):
             vectIni = actionMatrixOnVector( matrix , vectIni )
-            
-        for el in vectIni:
-            print(el[0]*100)
         return vectIni
     return -1
 
@@ -33,29 +30,12 @@ def multipleSlitQuantumExperiment(  ):
 def graphProbabilitiesVector( vector  ):
     data = len( vector )
     x = np.array([ x for x in range( data )])
-    y = np.array([ round(vector[x]*100,2) for x in range( data )])
+    y = np.array([ round(vector[x][0]*100,2) for x in range( data )])
 
     plot.bar( x,y , color ='g', align='center')
     plot.title('Probabilidades vector')
     plot.show()
     
-def main( ):
-    size, clicks = int( stdin.readline().strip() ), int( stdin.readline().strip() )
-    booleanMatrix = [ [ False for t in range( size )] for x in range( size  )]
-    vectIni = [ False for x in range( size ) ]
-##    vectIni[ 0 ] = True
-##    booleanMatrix[1][0] = True  
-##    booleanMatrix[1][5]=  True 
-##    booleanMatrix[2][0]= True  
-##    booleanMatrix[3][2]= True
-##    booleanMatrix[4][3]= True
-##    booleanMatrix[5][4]= True
-##
-##    print(clicks, experimentBooleanMatrix( clicks ,booleanMatrix, vectIni  ))
-##    print()
-##    print(booleanMatrix)
-##    for el in experimentBooleanMatrix( clicks ,booleanMatrix, vectIni  ):
-##        print( el )
-    
+
 
 #Author : Iván Camilo Rincón Saavedra
